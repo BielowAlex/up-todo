2fddedb6e970920424d140eac4fb8f9805786b86
BielowAlex <drohomyretskyi.oleksandr@gmail.com>
2024-04-09 20:43:53 +0200

    feat: update test for mainLayout

--- src/layouts/MainLayout/MainLayout.test.tsx
+++ src/layouts/MainLayout/MainLayout.test.tsx
@@ -1,8 +1,21 @@
-describe("MainLayout Component", () => {
-  it("Children", () => {
-    // appRender(<MainLayout><h1 data-testid="test-component">test</h1></MainLayout>)
-    //
-    // const currentComponent: HTMLElement = screen.getByTestId("test-component")
-    expect(true).toBe(true);
+import { vi } from "vitest";
+import { appRender } from "../../test/customRender.tsx";
+import { screen } from "@testing-library/react";
+import React from "react";
+import { MainLayout } from "./MainLayout.tsx";
+
+vi.mock("../../components", () => ({
+  Header: () => <header>Mocked Header</header>,
+  Footer: () => <footer>Mocked Footer</footer>,
+}));
+
+describe("MainLayout", () => {
+  it("renders Header, Outlet, and Footer components", () => {
+    const { container } = appRender(<MainLayout />);
+
+    expect(screen.getByText("Mocked Header")).toBeInTheDocument();
+    expect(screen.getByText("Mocked Footer")).toBeInTheDocument();
+
+    expect(container.querySelector(".container")).toBeInTheDocument();
   });
 });
