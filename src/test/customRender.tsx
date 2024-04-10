7100e8e973343696375dd632b06d61e8e151ac4f
BielowAlex <drohomyretskyi.oleksandr@gmail.com>
2024-04-10 15:00:01 +0200

    hotfix: redux types

--- src/test/customRender.tsx
+++ src/test/customRender.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import { render } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { setupStore } from "../store";
-
-const store = setupStore();
+import { persistor, store } from "../store";
+import { PersistGate } from "redux-persist/integration/react";
 
 export const appRender = (children: React.ReactNode) => {
   return render(
     <MemoryRouter>
-      <Provider store={store}>{children}</Provider>
+      <Provider store={store}>
+        <PersistGate persistor={persistor} loading={null}>
+          {children}
+        </PersistGate>
+      </Provider>
     </MemoryRouter>,
   );
 };
