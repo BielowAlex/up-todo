5c11f969df0837a2b996bad31959f79dc0c7a530
BielowAlex <drohomyretskyi.oleksandr@gmail.com>
2024-04-17 21:30:09 +0200

    fix: remove auth section

--- src/components/AuthSection/AuthSection.test.tsx
+++
@@ -1,5 +0,0 @@
-describe("AuthSection Component", () => {
-    it("test", () => {
-        expect(true).toBe(true)
-    })
-})
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 export * from "./Header";
 export * from "./SignUpMessage";
-export * from "./AuthSection";
 export * from "./Footer";
 export * from "./UI";
 export * from "./DateSection";
