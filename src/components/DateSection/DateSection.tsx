6b1ca333ebf58dbe7ec06d44acf95db132957cc2
BielowAlex <drohomyretskyi.oleksandr@gmail.com>
2024-04-18 11:52:17 +0200

    fix: data section

--- src/components/DateSection/DateSection.tsx
+++ src/components/DateSection/DateSection.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import style from "./style.module.scss";
 import { formatDate } from "../../utils/date/convert-title-date.util.ts";
 import { WeekCalendar } from "../WeekCalendar";
+import { useAppSelector } from "../../hooks";
 
 const DateSection: React.FC = () => {
-  const today: Date = new Date();
+  const today: string = useAppSelector(
+    (state) => state.dateReducer.selectedDate,
+  );
 
   return (
     <section className={style.container}>
-      <h2 className={style.title}>{formatDate(today)}</h2>
+      <h2 className={style.title}>{formatDate(new Date(today))}</h2>
       <WeekCalendar />
     </section>
   );
