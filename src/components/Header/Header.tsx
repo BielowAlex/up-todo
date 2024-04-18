fb3f0a700ac903ae7b45031aaf5f545c4d6cfcd9
BielowAlex <drohomyretskyi.oleksandr@gmail.com>
2024-04-18 12:01:01 +0200

    feat: create select menu button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,31 @@
 import React from "react";
 import style from "./style.module.scss";
 import { Logo } from "../Logo";
+import { SelectMenu, TextButton } from "../UI";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGear } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
+import { useAppDispatch } from "../../hooks";
+import { authActions, userActions } from "../../store";
 
 const Header: React.FC = () => {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const handleLogout = () => {
+    dispatch(authActions.logout());
+    dispatch(userActions.clearData());
+
+    navigate("/auth");
+  };
+
   return (
     <header className={style.container}>
       <Logo />
+      <SelectMenu buttonIcon={<FontAwesomeIcon icon={faGear} />}>
+        <TextButton>Settings</TextButton>
+        <TextButton>Privacy</TextButton>
+        <TextButton handleClick={handleLogout}>Logout</TextButton>
+      </SelectMenu>
     </header>
   );
 };
---
+++ src/components/UI/CustomComponents/SelectMenu/SelectMenu.test.tsx
@@ -0,0 +1,5 @@
+describe("SelectMenu Component", () => {
+  it("test", () => {
+    expect(true).toBe(true);
+  });
+});
