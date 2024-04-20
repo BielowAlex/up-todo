36d6248d59c000b2b2a910099fa67779ded52593
BielowAlex <drohomyretskyi.oleksandr@gmail.com>
2024-04-20 12:22:24 +0200

    feat: update Task

--- src/constant/task.constant.ts
+++ src/constant/task.constant.ts
@@ -1,29 +1,33 @@
 import { v4 } from "uuid";
-import { TaskStatusEnum } from "../types";
+import { Task, TaskStatusEnum } from "../types";
 
-export const taskConstant = [
+export const taskConstant: Task[] = [
   {
     id: v4(),
     title: "Task Title",
     description: "Create a responsive homepage design with a new color scheme",
-    status: TaskStatusEnum.NotStarted,
+    status: TaskStatusEnum.InProgress,
+    createdAt: "",
   },
   {
     id: v4(),
     title: "Design Homepage Layout",
     description: "Create a responsive homepage design with a new color scheme",
-    status: TaskStatusEnum.NotStarted,
+    status: TaskStatusEnum.InProgress,
+    createdAt: "",
   },
   {
     id: v4(),
     title: "Implement Login System",
     description: "Develop a secure login system with email and password",
     status: TaskStatusEnum.InProgress,
+    createdAt: "",
   },
   {
     id: v4(),
     title: "Database Migration",
     description: "Migrate the old user data to the new schema",
-    status: TaskStatusEnum.Completed,
+    status: TaskStatusEnum.InProgress,
+    createdAt: "",
   },
 ];
--- src/types/task.types.ts
+++ src/types/task.types.ts
@@ -1,5 +1,16 @@
+import { User } from "./user.types.ts";
+
 export enum TaskStatusEnum {
-  NotStarted = "NOT_STARTED",
   InProgress = "IN_PROGRESS",
   Completed = "COMPLETED",
+  Archived = "ARCHIVED",
 }
+
+export type Task = {
+  id: string;
+  title: string;
+  description: string;
+  createdAt: string;
+  status: TaskStatusEnum;
+  user?: User;
+};
