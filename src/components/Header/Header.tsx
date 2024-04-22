a00b8c21ba6a09f3f15de8b7ec3247035fba78c7
BielowAlex <drohomyretskyi.oleksandr@gmail.com>
2024-04-22 20:33:25 +0200

    fix: update header design

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import style from "./style.module.scss";
 import { Logo } from "../Logo";
+import { TextButton } from "../UI";
+import { faHandPointLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useNavigate } from "react-router-dom";
 
 const Header: React.FC = () => {
+  const navigate = useNavigate();
+
+  console.log(window.location.pathname);
+
   return (
     <header className={style.container}>
+      {window.location.pathname !== "/" && (
+        <TextButton handleClick={() => navigate(-1)} className={style.backBtn}>
+          <FontAwesomeIcon icon={faHandPointLeft} />
+        </TextButton>
+      )}
       <Logo />
     </header>
   );
